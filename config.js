6bdc09f250ba8abe6d1d8403253d0e85b75513ce
despreston <despreston@gmail.com>
2018-04-18 12:26:44 -0400

    init

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tradier: {
+    hostname: "sandbox.tradier.com"
+  },
+  db: {
+    dev: {
+      host: 'localhost',
+      port: 27017,
+      name: 'oitracker'
+    }
+  }
+};
