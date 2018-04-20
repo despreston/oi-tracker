a27e16526cf446d64e3cb3026de28ca4d5a6eb79
despreston <despreston@gmail.com>
2018-04-20 10:13:05 -0400

    Changing the layout to be more horizontal

--- www/src/components/main.vue
+++ www/src/components/main.vue
@@ -1,33 +1,60 @@
 <template>
   <div class="mt-3">
     <h3>DVAX Open Interest</h3>
-    <select class="form-control my-5">
-      <option
-        v-for="exp in expirations"
-        :value="exp"
-        :selected="exp.expiration_date === selected"
-      >
-        {{exp}}
-      </option>
-    </select>
+    <div class="form-inline my-5">
+      <label for="expiration">Expiration</label>
+      <select class="ml-2 form-control" name="expiration">
+        <option
+          v-for="exp in expirations"
+          :value="exp"
+          :selected="exp.expiration_date === selected"
+        >
+          {{exp}}
+        </option>
+      </select>
+    </div>
 
-    Calls
-    <table class="table mt-3">
-      <thead>
-        <tr>
-          <th>Strike</th>
-          <th>Current OI</th>
-          <th>Change (1d, 5d, 20d)</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="call of calls">
-          <td>{{call.strike}}</td>
-          <td>{{call.open_interest}}</td>
-          <td></td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="d-flex justify-content-around">
+      <div>
+        <h4>Calls</h4>
+        <table class="table table-striped mt-3">
+          <thead>
+            <tr>
+              <th>Strike</th>
+              <th>Current OI</th>
+              <th>Change (1d, 5d, 20d)</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="call of calls">
+              <td>{{call.strike}}</td>
+              <td>{{call.open_interest}}</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div>
+        <h4>Puts</h4>
+        <table class="table table-striped mt-3">
+          <thead>
+            <tr>
+              <th>Strike</th>
+              <th>Current OI</th>
+              <th>Change (1d, 5d, 20d)</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="put of puts">
+              <td>{{put.strike}}</td>
+              <td>{{put.open_interest}}</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
