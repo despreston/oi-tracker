e4c44dd3aef539f598c6a32f2543da1df1054910
despreston <despreston@gmail.com>
2018-04-25 15:09:30 -0400

    ebs config stuff

--- config.js
+++ config.js
@@ -8,6 +8,13 @@ module.exports = {
       host: 'localhost',
       port: 27017,
       name: 'oitracker'
+    },
+    production: {
+      host: ''
+      port: 27017,
+      name: 'oitracker',
+      username: 'production',
+      password: 'i8EwXyTYb6dYJPZ3'
     }
   }
 };
